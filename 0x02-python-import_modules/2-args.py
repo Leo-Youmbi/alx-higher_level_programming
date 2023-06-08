3f79482e183787046906e7fd1540474395bac046
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-06-08 08:40:11 +0100

    Changing to executables

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    args = sys.argv[1:]
+    numArgs = len(args)
+
+    if numArgs == 0:
+        print("0 arguments.")
+    elif numArgs == 1:
+        print("1 argument:")
+    else:
+        for i in range(numArgs):
+            print("{}: {}".format(i + 1, args[0]))
