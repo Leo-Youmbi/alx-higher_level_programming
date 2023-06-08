49cac3eb183730a1e1b61cd7489946e263584232
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-06-08 13:40:21 +0100

    Implementing my_calculator.py 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,23 +10,13 @@ if __name__ == "__main__":
 
     if numArgs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(0)
+        sys.exit(1)
     fnum = int(args[0])
     op = args[1]
     snum = int(args[2])
-    match op:
-        case '+':
-            print("{} + {} = {}".format(fnum, snum, add(fnum, snum)))
-            sys.exit(0)
-        case '-':
-            print("{} - {} = {}".format(fnum, snum, sub(fnum, snum)))
-            sys.exit(0)
-        case '*':
-            print("{} * {} = {}".format(fnum, snum, mul(fnum, snum)))
-            sys.exit(0)
-        case '/':
-            print("{} / {} = {}".format(fnum, snum, div(fnum, snum)))
-            sys.exit(0)
-        case _:
-            print("Unknown operator:  Available operators: +, -, * and /")
-            sys.exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in ops.keys():
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{} {} {} = {}".format(fnum, op, snum, ops[op](fnum, snum)))
+    sys.exit(0)
