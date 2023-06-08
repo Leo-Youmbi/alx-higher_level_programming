6a7464ce9423aa79aaf0c72b2588ec75eb1263db
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-06-08 13:30:30 +0100

    Implementing my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,4 +5,28 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    
+    args = sys.argv[1:]
+    numArgs = len(args)
+
+    if numArgs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(0)
+    fnum = int(args[0])
+    op = args[1]
+    snum = int(args[2])
+    match op:
+        case '+':
+            print("{} + {} = {}".format(fnum, snum, add(fnum, snum)))
+            sys.exit(0)
+        case '-':
+            print("{} - {} = {}".format(fnum, snum, sub(fnum, snum)))
+            sys.exit(0)
+        case '*':
+            print("{} * {} = {}".format(fnum, snum, mul(fnum, snum)))
+            sys.exit(0)
+        case '/':
+            print("{} / {} = {}".format(fnum, snum, div(fnum, snum)))
+            sys.exit(0)
+        case _:
+            print("Unknown operator:  Available operators: +, -, * and /")
+            sys.exit(1)
