b9b1783a254c7353a4782938be7799befa633998
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-06-14 11:51:21 +0100

    Implementing mandatory tasks 3

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+def complex_delete(a_dictionary: dict, value):
+    newDict = a_dictionary
+    keys = a_dictionary.copy().keys()
+    for key in keys:
+        if newDict[key] == value:
+            del newDict[key]
+    return newDict
