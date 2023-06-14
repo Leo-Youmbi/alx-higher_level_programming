62176b77c383b0aefffdeeb0fe51caf36f1dc574
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-06-14 11:55:51 +0100

    Implementing mandatory tasks 3

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def complex_delete(a_dictionary: dict, value):
     newDict = a_dictionary
     keys = a_dictionary.copy().keys()
--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    newList = list(map(lambda i: i*number, my_list))
-    return newList
+    return list(map(lambda i: i*number, my_list))
