5c7d01dbf2ee1232c7ffa20dd98ac591cd86280a
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-06-14 11:26:54 +0100

    Implementing mandatory tasks 2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    tup = tuple(a_dictionary.items())
-    best = tup[0][1]
-    for i in range(1, len(tup)):
-        if tup[i][1] > best:
-            best = tup[i][1]
-            bestStudent = tup[i][0]
-    return bestStudent
+    return (max(a_dictionary, key=a_dictionary.get))
