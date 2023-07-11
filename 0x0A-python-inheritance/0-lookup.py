8acdcec319b969c241c02feda4207d14167581fa
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-07-11 11:52:52 +0100

    Python Inheritance Tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bon/python3
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes.
+
+    Args:
+        obj (Any): Object
+
+    Returns:
+        list: Object's attributes and members
+    """
+    return (dir(obj))
