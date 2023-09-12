c5d6d7afd7c6bf5c4f373f7f9c33922a9cd6be53
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-09-12 15:57:03 +0100

    Javascript - Objects, Scopes and Closures Advanced Tasks

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,7 +2,7 @@
 const dict = require('./utils/101-data').dict;
 const newArray = {};
 for (const key in dict) {
-  if (!newArray.hasOwnProperty(dict[key])) {
+  if (newArray[dict[key]] === undefined) {
     newArray[dict[key]] = [];
   }
   newArray[dict[key]].push(key);
