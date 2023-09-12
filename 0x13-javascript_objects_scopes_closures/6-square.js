d072f09f88b501cb61428be50c62e20a67f41b2f
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-09-12 14:40:46 +0100

    Javascript - Objects, Scopes and Closures Tasks

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Square5 = require('./5-square');
 
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends Square5 {
   constructor (size) {
     super(size, size);
   }
