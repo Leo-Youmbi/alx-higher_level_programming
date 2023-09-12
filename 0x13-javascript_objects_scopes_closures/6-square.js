024c3414bc6a008cf9dc84b498491526486c5660
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-09-12 14:36:35 +0100

    Javascript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (char = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str += char;
+      }
+      console.log(str);
+    }
+  }
+};
