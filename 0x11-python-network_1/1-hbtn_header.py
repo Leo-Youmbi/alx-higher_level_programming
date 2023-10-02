2d2e41317427ddc1227dd1370e8f42da1922d08b
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-02 19:50:21 +0100

    feat: python-network_1 mandatory tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,8 @@ header of the response."""
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
 if __name__ == '__main__':
+    url = sys.argv[1]
+
     with urllib.request.urlopen(url) as response:
         print(response.headers['X-Request-Id'])
