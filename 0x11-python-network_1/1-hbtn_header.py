70df2a3688f1b188b2126a99a36c8aafb58f59ae
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-02 19:45:56 +0100

    feat: python-network_1 mandatory tasks

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found in the
+header of the response."""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as response:
+        print(response.headers['X-Request-Id'])
